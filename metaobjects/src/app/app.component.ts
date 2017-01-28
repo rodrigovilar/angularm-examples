3a25f5755e1892b1978f883b9fe3b9076a1719a6
Rodrigo Vilar <rvilar@gmail.com>
2017-01-28 14:01:43 -0300

    Doing for loops on entity types and property types

--- metaobjects/src/app/app.component.ts
+++ metaobjects/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+import { AngularmService, EntityTypesComponent } from 'angularm';
+
+import { describeDomain } from './entities';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent extends EntityTypesComponent {
+
+  constructor(
+    private angularm: AngularmService
+  ) {
+    super();
+    describeDomain(this.angularm);
+    this.entityTypes = this.angularm.listEntityTypes();
+  }
+
 }
--- metaobjects/src/app/app.module.ts
+++ metaobjects/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { SampleModule } from 'angularm';
+import { AngularmModule } from 'angularm';
 
 import { AppComponent } from './app.component';
 
@@ -15,7 +15,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    SampleModule
+    AngularmModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
